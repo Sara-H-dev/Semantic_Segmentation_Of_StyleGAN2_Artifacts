c864a55d4d0156e1ee9f4e0e8b75457703ea79d3
Sara Heyd <sara.heyd@st.oth-regensburg.de>
2025-09-23 12:27:33 +0200

    Changed the forward part of the TverskyLoss so that it assures that the groundtruth is between 0 and 1 and the y_pred that enter als Logits are going through a sigmoid function to be parsed to porbability. In addition the mean of tversky get's returned, because a scala not a verctor is needed.

--- loss/TverskyLoss.py
+++ loss/TverskyLoss.py
@@ -18,14 +18,34 @@ class TverskyLoss_binary(nn.Module):
                         # so TP = 0 and FN = 0
                         # but FP gets ignort
 
-        torch.flatten(y_pred)
-        torch.flatten(y_gtruth)
+        if y_gtruth.dim() == 3:  # (B,H,W) -> (B,1,H,W)
+            y_gtruth = y_gtruth.unsqueeze(1)     
 
-        TP = torch.sum(y_gtruth * y_pred)
-        FP = torch.sum((1 - y_gtruth) * y_pred)
-        FN = torch.sum(y_gtruth * (1 - y_pred))
+        y_gtruth = y_gtruth.float()
 
-        tversky = 1 - (TP + smooth) / (TP + self.alpha * FP, self.beta * FN + self.smooth)
-        return tversky
+        # normalize labels if {0,255}
+        if y_gtruth.max() > 1:
+            y_gtruth = y_gtruth / 255.0  
+
+
+        # Logits -> propability
+        y_pred = torch.sigmoid(y_pred)
+
+        # Flatten per Sample
+        # to dimension (B, N)
+        # B = Batchsize, N = number of pixel per picture
+        y_pred   = y_pred.view(y_pred.size(0), -1)
+        y_gtruth = y_gtruth.view(y_gtruth.size(0), -1)
+
+        # .sum(dim=1) sums over the pixel dimension N.
+        # output dim = B, one value per image
+        TP = (y_gtruth * y_pred).sum(dim=1)
+        FP = ((1.0 - y_gtruth) * y_pred).sum(dim=1)
+        FN = (y_gtruth * (1.0 - y_pred)).sum(dim=1)
+
+        tversky = 1.0 - ((TP + smooth) / (TP + self.alpha * FP + self.beta * FN + smooth))
+        # mean other all pictures
+        # = one value
+        return tversky.mean()
     
     
